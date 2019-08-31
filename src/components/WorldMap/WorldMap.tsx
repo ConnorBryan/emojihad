83ba8117b212b72e573c8efcd9a4ad7c2b288336
Connor Bryan <cchromium@gmail.com>
2019-08-31 13:07:24 -0500

    Use CSS grid for WorldMap

--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -1,13 +1,8 @@
+import flatten from "lodash.flatten";
 import React from "react";
 import styled, { css } from "styled-components";
 
-import {
-  getSpaceUp,
-  getSpaceRight,
-  getSpaceDown,
-  getSpaceLeft,
-  styleWhen
-} from "../../helpers";
+import { styleWhen } from "../../helpers";
 import { WorldMap as WorldMapType } from "../../types";
 import { Emoji } from "../Emoji";
 
@@ -17,34 +12,15 @@ interface IProps {
 
 export default function WorldMap({ map }: IProps) {
   return (
-    <StyledWorldMap>
+    <StyledWorldMap columns={map[0].length}>
       <div className="-inner">
-        {map.map((row, yIndex) => {
-          return (
-            <div key={yIndex} className="-spaceRowWrapper">
-              {row.map(({ uuid, space }, xIndex) => {
-                const hasUpBorder = !getSpaceUp(map, yIndex, xIndex);
-                const hasRightBorder = !getSpaceRight(map, yIndex, xIndex);
-                const hasDownBorder = !getSpaceDown(map, yIndex, xIndex);
-                const hasLeftBorder = !getSpaceLeft(map, yIndex, xIndex);
-                const isEmpty = space === "⚪️";
+        {flatten(map).map(({ space }, index) => {
+          const isEmpty = space === "⚪️";
 
-                return (
-                  <StyledSpace
-                    key={uuid}
-                    isEmpty={isEmpty}
-                    hasUpBorder={hasUpBorder}
-                    hasRightBorder={hasRightBorder}
-                    hasDownBorder={hasDownBorder}
-                    hasLeftBorder={hasLeftBorder}
-                  >
-                    {!isEmpty && (
-                      <Emoji className="-space" emoji={space} size={64} />
-                    )}
-                  </StyledSpace>
-                );
-              })}
-            </div>
+          return (
+            <StyledSpace key={index} isEmpty={isEmpty}>
+              {!isEmpty && <Emoji emoji={space} size={64} />}
+            </StyledSpace>
           );
         })}
       </div>
@@ -52,93 +28,61 @@ export default function WorldMap({ map }: IProps) {
   );
 }
 
-const StyledWorldMap = styled.div<any>`
-  width: 130vw;
-  height: 100vh;
-  z-index: 1;
-  background: rgba(0, 0, 0, 0.7);
-  overflow: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const StyledWorldMap = styled.div<{ columns: number }>`
+  ${({ columns }) =>
+    css`
+      width: 130vw;
+      height: 100vh;
+      z-index: 1;
+      background: rgba(0, 0, 0, 0.7);
+      overflow: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
 
-  .-inner {
-    @keyframes wavering {
-      0% {
-        transform: rotateX(50deg) rotateY(0deg) rotateZ(15deg);
-      }
-      50% {
-        transform: rotateX(54deg) rotateY(0deg) rotateZ(19deg);
-      }
-      100% {
-        transform: rotateX(50deg) rotateY(0deg) rotateZ(15deg);
-      }
-    }
+      .-inner {
+        @keyframes wavering {
+          0% {
+            transform: rotateX(50deg) rotateY(0deg) rotateZ(15deg);
+          }
+          50% {
+            transform: rotateX(54deg) rotateY(0deg) rotateZ(19deg);
+          }
+          100% {
+            transform: rotateX(50deg) rotateY(0deg) rotateZ(15deg);
+          }
+        }
 
-    background: darkgreen;
-    border: 4px solid black;
-    border-radius: 5px;
-    padding: 20px;
-    animation: wavering 3s infinite;
-  }
-  .-spaceRowWrapper {
-    display: flex;
-  }
+        display: grid;
+        grid-template-columns: repeat(${columns}, 80px);
+        background: darkgreen;
+        border: 4px solid black;
+        border-radius: 5px;
+        padding: 20px;
+        animation: wavering 3s infinite;
+      }
+    `}
 `;
 
 const StyledSpace = styled.div<any>`
-  ${props => {
-    const {
-      hasUpBorder,
-      hasRightBorder,
-      hasDownBorder,
-      hasLeftBorder,
-      isEmpty
-    } = props;
-
-    return css`
-      flex: 0 0 80px;
+  ${({ isEmpty }) =>
+    css`
       display: flex;
       align-items: center;
       justify-content: center;
       background: tan;
       width: 80px;
       height: 80px;
-      padding: 12px;
       transition: background 0.2s ease-in-out;
 
       ${styleWhen(
         !isEmpty,
         css`
           cursor: pointer;
-          
+
           &:hover {
             background: #ffe7bf;
-          } 
-          ${styleWhen(
-            hasUpBorder,
-            css`
-              border-top: 4px solid;
-            `
-          )}
-          ${styleWhen(
-            hasRightBorder,
-            css`
-              border-right: 4px solid;
-            `
-          )}
-          ${styleWhen(
-            hasDownBorder,
-            css`
-              border-bottom: 4px solid;
-            `
-          )}
-          ${styleWhen(
-            hasLeftBorder,
-            css`
-              border-left: 4px solid;
-            `
-          )}
+          }
         `
       )}
       ${styleWhen(
@@ -147,6 +91,5 @@ const StyledSpace = styled.div<any>`
           background: rgba(0, 0, 0, 0.1);
         `
       )}
-    `;
-  }}
+    `}
 `;
